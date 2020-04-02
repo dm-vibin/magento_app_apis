c26afb7c9713c46838ad5e51e7f19a359bd631c3
dm-vibin <vibin@digitalmesh.com>
2020-04-02 10:38:28 +0530

    New APIs added change password

--- api_project.js
+++ api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-04-02T04:51:07.993Z",
+    "time": "2020-04-02T05:04:51.988Z",
     "url": "http://apidocjs.com",
     "version": "0.20.1"
   }
