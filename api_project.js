758621aeed9220b89d1da184ea4b0dabdb0ae2e6
Manu Antony <manu@digitalmesh.com>
2020-04-22 13:49:56 +0530

    removed 'email' from update user

--- api_project.js
+++ api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-04-22T03:36:48.674Z",
+    "time": "2020-04-22T08:17:48.764Z",
     "url": "http://apidocjs.com",
     "version": "0.20.1"
   }
