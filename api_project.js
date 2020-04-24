fb2caf1b00d3fe2a812300b66b3cedf308118901
Manu Antony <manu@digitalmesh.com>
2020-04-24 09:43:23 +0530

    Added 'pageWise' data nodes in GetDashboard, added pagination nodes 'page' and 'hasNext' in params and responses

--- api_project.js
+++ api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-04-23T02:30:29.291Z",
+    "time": "2020-04-24T04:11:08.610Z",
     "url": "http://apidocjs.com",
     "version": "0.20.1"
   }
